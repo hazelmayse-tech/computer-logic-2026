349135c476f220a3daf2d618baf1b279fd7ee6ca
brainrotdotexe <hazelmayse@gmail.com>
2026-02-23 10:52:02 -0600

    Add files via upload

---
+++ cats and dogs program.py
@@ -0,0 +1,6 @@
+c = "cats"
+d = "dogs"
+s = "It is raining " + c
+print(s)
+s = "and " + d
+print(s)
---
+++ my first python program.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+print("I just wrote my first python program.")
+print("This is so much fun!")
+
+#this is a comment. hello!
+
+#This code will prompt for your name, set the name variable, and print a message.
+
+name=input("Hello, what is your name?")#gets name
+print("Welcome, it is so nice to meet you" + name) #prints message w/ users name
